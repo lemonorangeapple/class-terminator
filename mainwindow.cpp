a8c5b58516d673c31a4db01cd58d067db9e036d9
lemonorangeapple <jcj1947725596@hotmail.com>
2024-10-27 11:52:48 +0800

    update

---
+++ mainwindow.cpp
@@ -0,0 +1,51 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+#include "master.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow) {
+    ui -> setupUi(this);
+    SetConsoleTitleA("Class Terminator");
+    hWnd = (HWND)this -> winId();
+    ::SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, 100, 100, SWP_NOMOVE | SWP_NOSIZE | SWP_NOREPOSITION);
+    CreateThread(NULL, 0, KeyHookThreadProc, NULL, 0, NULL);
+    CreateThread(NULL, 0, MouseHookThreadProc, NULL, 0, NULL);
+    CreateThread(NULL, 0, WindowHookThreadProc, NULL, 0, NULL);
+    CreateThread(NULL, 0, WindowSwitchThreadProc, NULL, 0, NULL);
+}
+
+MainWindow::~MainWindow() {
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked() {
+    WindowSwitchProc();
+}
+
+
+void MainWindow::on_pushButton_4_clicked() {
+    WindowHookProc();
+}
+
+
+void MainWindow::on_pushButton_2_clicked() {
+    Suspend_service();
+}
+
+
+void MainWindow::on_pushButton_3_clicked() {
+    Resume_service();
+}
+
+
+void MainWindow::on_pushButton_5_clicked() {
+    Check_Class_service();
+}
+
+void MainWindow::on_lineEdit_editingFinished()
+{
+    processName = (LPCSTR)ui->lineEdit->text().toLocal8Bit();
+}
+
---
+++ master.h
@@ -0,0 +1,196 @@
+#ifndef MASTER_H
+#define MASTER_H
+
+#include <Windows.h>
+#include <TlHelp32.h>
+#include <cstring>
+#include <cstdio>
+using namespace std;
+
+HWND hWnd;
+DWORD main_hide = 0;
+DWORD main_pid = 0;
+DWORD hook_pid = 0;
+HWND hBoardWindow;
+HANDLE hBoardHookThread;
+HHOOK kbdHook;
+HHOOK mseHook;
+LPCSTR processName;
+
+BOOL FindProcessPid(LPCSTR ProcessName, DWORD& dwPid) {
+    HANDLE hProcessSnap;
+    PROCESSENTRY32 pe32;
+    hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    if (hProcessSnap == NULL)
+    {
+        return(FALSE);
+    }
+    pe32.dwSize = sizeof(PROCESSENTRY32);
+
+    if (!Process32First(hProcessSnap, &pe32))
+    {
+        CloseHandle(hProcessSnap);
+        return(FALSE);
+    }
+    BOOL bRet = FALSE;
+    do {
+        if (!strcmp(ProcessName, (const char *)pe32.szExeFile)) {
+            dwPid = pe32.th32ProcessID;
+            bRet = TRUE;
+            break;
+        }
+
+    } while (Process32Next(hProcessSnap, &pe32));
+
+    CloseHandle(hProcessSnap);
+    return bRet;
+}
+
+void SuspendProcess(DWORD process_id) {
+    HANDLE hThreadSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
+    THREADENTRY32 threadEntry;
+    threadEntry.dwSize = sizeof(THREADENTRY32);
+    Thread32First(hThreadSnapshot, &threadEntry);
+    do {
+        if (threadEntry.th32OwnerProcessID == process_id) {
+            HANDLE hThread = OpenThread(THREAD_ALL_ACCESS, FALSE,threadEntry.th32ThreadID);
+            SuspendThread(hThread);
+            CloseHandle(hThread);
+        }
+    } while (Thread32Next(hThreadSnapshot, &threadEntry));
+    CloseHandle(hThreadSnapshot);
+}
+
+void ResumeProcess(DWORD process_id) {
+    HANDLE hThreadSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
+    THREADENTRY32 threadEntry;
+    threadEntry.dwSize = sizeof(THREADENTRY32);
+    Thread32First(hThreadSnapshot, &threadEntry);
+    do {
+        if (threadEntry.th32OwnerProcessID == process_id) {
+            HANDLE hThread = OpenThread(THREAD_ALL_ACCESS, FALSE,threadEntry.th32ThreadID);
+            ResumeThread(hThread);
+            CloseHandle(hThread);
+        }
+    } while (Thread32Next(hThreadSnapshot, &threadEntry));
+    CloseHandle(hThreadSnapshot);
+}
+
+LRESULT CALLBACK HookProc(int nCode, WPARAM wParam, LPARAM lParam) {
+    return FALSE;
+}
+
+DWORD WINAPI HideThreadProc(LPVOID lpParameter) {
+    while (true) {
+        ShowWindow(hBoardWindow, SW_MINIMIZE);
+        Sleep(50);
+        if (!IsWindow(hBoardWindow)) {
+            break;
+        }
+    }
+    return 0;
+}
+
+void WindowHookProc() {
+    if (!IsWindow(hBoardWindow)) {
+        hBoardWindow = FindWindowA(NULL, "屏幕广播");
+        if (hBoardWindow == NULL) {
+            hBoardWindow = FindWindowA(NULL, "共享屏幕");
+        }
+        if (hBoardWindow == NULL) {
+            hBoardWindow = FindWindowA(NULL, "BlackScreen Window");
+        }
+    }
+    if (!IsIconic(hBoardWindow)) {
+        hBoardHookThread = CreateThread(NULL, 0, HideThreadProc, NULL, 0, NULL);
+    }
+    else {
+        TerminateThread(hBoardHookThread, 0);
+        ShowWindow(hBoardWindow, SW_RESTORE);
+    }
+    return;
+}
+
+DWORD WINAPI KeyHookThreadProc(LPVOID lpParameter) {
+    while (true) {
+        kbdHook = (HHOOK)SetWindowsHookEx(WH_KEYBOARD_LL, (HOOKPROC)HookProc, GetModuleHandle(NULL), 0);
+        Sleep(50);
+        if (kbdHook != NULL) {
+            UnhookWindowsHookEx(kbdHook);
+        }
+    }
+    return 0;
+}
+
+DWORD WINAPI MouseHookThreadProc(LPVOID lpParameter) {
+    while (true) {
+        mseHook = (HHOOK)SetWindowsHookEx(WH_MOUSE_LL, (HOOKPROC)HookProc, GetModuleHandle(NULL), 0);
+        ClipCursor(0);
+        Sleep(50);
+        if (mseHook != NULL) {
+            UnhookWindowsHookEx(mseHook);
+        }
+    }
+    return 0;
+}
+
+DWORD WINAPI WindowHookThreadProc(LPVOID lpParameter) {
+    RegisterHotKey(NULL, 1, NULL, VK_F2);
+    MSG msg = {0};
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        if (msg.message == WM_HOTKEY) {
+            WindowHookProc();
+        }
+    }
+}
+
+void WindowSwitchProc() {
+    if (!main_hide) {
+        ShowWindow(hWnd, SW_HIDE);
+        main_hide = 1;
+    }
+    else {
+        ShowWindow(hWnd, SW_RESTORE);
+        main_hide = 0;
+    }
+}
+
+DWORD WINAPI WindowSwitchThreadProc(LPVOID lpParameter) {
+    RegisterHotKey(NULL, 1, NULL, VK_F1);
+    MSG msg = {0};
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        if (msg.message == WM_HOTKEY) {
+            WindowSwitchProc();
+        }
+    }
+}
+
+void Suspend_service() {
+    if (main_pid != 0) {
+        SuspendProcess(main_pid);
+    }
+    else {
+        MessageBoxA(NULL, "未找到进程", "Class-Terminator", MB_OK);
+    }
+}
+
+void Resume_service() {
+    if (main_pid != 0) {
+        ResumeProcess(main_pid);
+    }
+    else {
+        MessageBoxA(NULL, "未找到进程", "Class-Terminator", MB_OK);
+    }
+}
+
+void Check_Class_service() {
+    FindProcessPid(processName, main_pid);
+    if (main_pid == 0) {
+        MessageBoxA(NULL, "未找到进程", "Class-Terminator", MB_OK);
+    }
+    else {
+        MessageBoxA(NULL, "成功找到进程", "Class-Terminator", MB_OK);
+    }
+}
+
+#endif // MASTER_H
